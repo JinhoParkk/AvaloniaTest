91731f93a3d61411e34365eea69581dcd96a01ed
박진호 <pjh3@paytalab.com>
2025-12-25 14:46:49 +0900

    ㅋㅋ

--- JinoOrder/Presentation/Main/MainView.axaml.cs
+++ JinoOrder/Presentation/Main/MainView.axaml.cs
@@ -1,7 +1,4 @@
-using System;
 using Avalonia.Controls;
-using JinoOrder.Presentation.Common;
-using JinoOrder.Presentation.Main;
 
 namespace JinoOrder.Presentation.Main;
 
@@ -10,43 +7,5 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
-
-        DataContextChanged += OnDataContextChanged;
-    }
-
-    private void OnDataContextChanged(object? sender, EventArgs e)
-    {
-        if (DataContext is MainViewModel viewModel)
-        {
-            viewModel.MinimizeRequested += OnMinimizeRequested;
-            viewModel.MaximizeRequested += OnMaximizeRequested;
-            viewModel.CloseRequested += OnCloseRequested;
-        }
-    }
-
-    private void OnMinimizeRequested()
-    {
-        var window = TopLevel.GetTopLevel(this) as Window;
-        if (window != null)
-        {
-            window.WindowState = WindowState.Minimized;
-        }
-    }
-
-    private void OnMaximizeRequested()
-    {
-        var window = TopLevel.GetTopLevel(this) as Window;
-        if (window != null)
-        {
-            window.WindowState = window.WindowState == WindowState.Maximized
-                ? WindowState.Normal
-                : WindowState.Maximized;
-        }
-    }
-
-    private void OnCloseRequested()
-    {
-        var window = TopLevel.GetTopLevel(this) as Window;
-        window?.Close();
     }
 }
--- JinoOrder/Presentation/Shell/JinoOrderMainView.axaml.cs
+++ JinoOrder/Presentation/Shell/JinoOrderMainView.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 
 namespace JinoOrder.Presentation.Shell;
@@ -7,5 +8,42 @@ public partial class JinoOrderMainView : UserControl
     public JinoOrderMainView()
     {
         InitializeComponent();
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        if (DataContext is JinoOrderMainViewModel viewModel)
+        {
+            viewModel.MinimizeRequested += OnMinimizeRequested;
+            viewModel.MaximizeRequested += OnMaximizeRequested;
+            viewModel.CloseRequested += OnCloseRequested;
+        }
+    }
+
+    private void OnMinimizeRequested()
+    {
+        var window = TopLevel.GetTopLevel(this) as Window;
+        if (window != null)
+        {
+            window.WindowState = WindowState.Minimized;
+        }
+    }
+
+    private void OnMaximizeRequested()
+    {
+        var window = TopLevel.GetTopLevel(this) as Window;
+        if (window != null)
+        {
+            window.WindowState = window.WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
+    }
+
+    private void OnCloseRequested()
+    {
+        var window = TopLevel.GetTopLevel(this) as Window;
+        window?.Close();
     }
 }
